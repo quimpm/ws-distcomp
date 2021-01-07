bdc0f8a643a5b43dedc80085fd8898627dc68bdf
Sergi Simon Balcells <sergisiba@gmail.com>
2021-01-07 04:26:16 +0100

    Owner is implicit now.

--- myexam/exam/serializers.py
+++ myexam/exam/serializers.py
@@ -6,3 +6,11 @@ class ExamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exam
         fields = ("id", "description", "date", "location")
+
+    def create(self, validated_data):
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+            return super().create(validated_data | {"owner": user})
+        raise TypeError("User is not authenticated!")
