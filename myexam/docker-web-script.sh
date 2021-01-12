e007d756ac92c44087413863d74a5cca8d4e777b
Sergi Simon Balcells <sergisiba@gmail.com>
2021-01-12 16:34:38 +0100

    Added spaces to remove ^M (\r added for windows.)

--- myexam/docker-web-script.sh
+++ myexam/docker-web-script.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-python3 manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:8000 myexam.wsgi
+#!/usr/bin/env bash 
+python3 manage.py collectstatic --no-input 
+gunicorn --bind 0.0.0.0:8000 myexam.wsgi 
